13dc02b691d436659071e60d813bde7ca3fff722
Jie Shen <yeapjieshen@hotmail.com>
2024-11-26 12:37:52 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "flask-rds2.cd86qo0gclor.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "yeapjieshen-employee"
 customregion = "us-east-1"
 
