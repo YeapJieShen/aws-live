80c7db9d57f26f1032138cc5b0fec483b6492bbc
Jie Shen <yeapjieshen@hotmail.com>
2024-12-04 13:26:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 customhost = "database-2.cd86qo0gclor.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "abc12345"
-customdb = "employee"
-custombucket = "yeapjieshen-employee"
+customdb = "database_2"
+custombucket = "yeapjieshen-anotherbucket"
 customregion = "us-east-1"
 
